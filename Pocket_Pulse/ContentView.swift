f36784d936a2821ab7b58dbd096a4f4045f21215
tenalisriharsha <tenalisriharsha@gmail.com>
2026-01-09 13:21:25 -0600

    Initial Commit

---
+++ Pocket_Pulse/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Pocket_Pulse
+//
+//  Created by Tenali Sri Harsha on 1/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Pocket_Pulse/Pocket_PulseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pocket_PulseApp.swift
+//  Pocket_Pulse
+//
+//  Created by Tenali Sri Harsha on 1/9/26.
+//
+
+import SwiftUI
+
+@main
+struct Pocket_PulseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
